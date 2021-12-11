8e2b67d2224b1a52042c80a24329494c5f25a836
David Ferguson (WINDOWS) <davfer@microsoft.com>
2021-12-11 11:46:37 -0800

    probe drives, node, cl.exe

--- test.cmd
+++ test.cmd
@@ -8,17 +8,71 @@
     where ninja
 
     echo.
+    echo cmake version ---------------------------------------------------------
+    cmake --version
+
+    echo.
+    echo ninja version ---------------------------------------------------------
+    ninja --version
+
+    echo.
+    echo node version ----------------------------------------------------------
+    node --version
+
+    echo.
+    echo run a node script -----------------------------------------------------
+    node tasks version
+
+    echo.
     echo set -------------------------------------------------------------------
     set
 
     echo.
-    echo SystemRoot -----------------------------------------------------------
+    echo Current Dir ----------------------------------------------------------
+    dir /a
+
+    echo.
+    echo SystemRoot ------------------------------------------------------------
     dir /a/b "%SystemRoot%\.."
 
     echo.
-    echo ProgramFiles -----------------------------------------------------------
+    echo ProgramFiles ----------------------------------------------------------
     dir /a/b "%ProgramFiles%"
 
     echo.
-    echo ProgramFiles -----------------------------------------------------------
+    echo ProgramFiles x86 ------------------------------------------------------
     dir /a/b "%ProgramFiles(x86)%"
+
+    echo.
+    echo Drives ----------------------------------------------------------------
+    dir a:\
+    dir b:\
+    dir c:\
+    dir d:\
+    dir e:\
+    dir f:\
+    dir g:\
+    dir h:\
+    dir i:\
+    dir j:\
+    dir k:\
+    dir l:\
+    dir m:\
+    dir n:\
+    dir o:\
+    dir p:\
+    dir q:\
+    dir r:\
+    dir s:\
+    dir t:\
+    dir u:\
+    dir v:\
+    dir w:\
+    dir x:\
+    dir y:\
+    dir z:\
+
+    echo.
+    echo cl.exe ----------------------------------------------------------------
+    pushd "c:\Program Files (x86)\Microsoft Visual Studio"
+    dir /s/b cl.exe
