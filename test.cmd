3f74d0cb433ad1776f6dd33c6ef5f26783e0f0d9
David Ferguson (WINDOWS) <davfer@microsoft.com>
2021-12-11 11:24:08 -0800

    scan some dirs

--- test.cmd
+++ test.cmd
@@ -1,4 +1,24 @@
 @if :%_echo%==:1 (echo on) else (echo off)
     setlocal enableextensions
     echo You are running the test.cmd script
-    echo output to a file > log1.txt
+
+    where node
+    where python
+    where cmake
+    where ninja
+
+    echo.
+    echo set -------------------------------------------------------------------
+    set
+
+    echo.
+    echo SystemRoot -----------------------------------------------------------
+    dir /a/b "%SystemRoot%\.."
+
+    echo.
+    echo ProgramFiles -----------------------------------------------------------
+    dir /a/b "%ProgramFiles%"
+
+    echo.
+    echo ProgramFiles -----------------------------------------------------------
+    dir /a/b "%ProgramFiles(x86)%"
